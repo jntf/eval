a814cb0ba75590afc97b076cbb1385a176716d1c
julien <107534950+jntf@users.noreply.github.com>
2023-04-15 08:41:45 +0200

    new amplify project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "evalio": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "function": {
+    "evalPredict": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ src/graphql/mutations.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createVehicle = /* GraphQL */ `
+  mutation CreateVehicle(
+    $input: CreateVehicleInput!
+    $condition: ModelVehicleConditionInput
+  ) {
+    createVehicle(input: $input, condition: $condition) {
+      make
+      model
+      keywords
+      energies
+      transmissions
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateVehicle = /* GraphQL */ `
+  mutation UpdateVehicle(
+    $input: UpdateVehicleInput!
+    $condition: ModelVehicleConditionInput
+  ) {
+    updateVehicle(input: $input, condition: $condition) {
+      make
+      model
+      keywords
+      energies
+      transmissions
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteVehicle = /* GraphQL */ `
+  mutation DeleteVehicle(
+    $input: DeleteVehicleInput!
+    $condition: ModelVehicleConditionInput
+  ) {
+    deleteVehicle(input: $input, condition: $condition) {
+      make
+      model
+      keywords
+      energies
+      transmissions
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
