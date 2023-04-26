8a8ee3575b0adb45c902b11ba7a403fcd6ac1737
julien <107534950+jntf@users.noreply.github.com>
2023-04-26 16:30:46 +0200

    Update userpanel and route

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,47 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Analyse from "../views/Analyse.vue";
 import Home from "../views/Home.vue";
-import Eval from "../views/EvalView.vue";
+import Login from "../views/auth/Login.vue";
+import User from "../views/user/IndexUser.vue";
+import Analyse from "../views/user/Analyse.vue";
+import Eval from "../views/user/EvalView.vue";
+import { Auth } from 'aws-amplify';
+
+const requireAuth = (to, from, next) => {
+  Auth.currentAuthenticatedUser()
+    .then(() => {
+      // User is authenticated, allow access to route
+      next();
+    })
+    .catch(() => {
+      // User is not authenticated, redirect to home page
+      next({ name: "home" });
+    });
+};
+
+// Ajouter la redirection vers /user/analyse
+const requireAuthWithRedirection = (to, from, next) => {
+  Auth.currentAuthenticatedUser()
+    .then(() => {
+      // User is authenticated, redirect to analysis page
+      next({ name: "analyse" });
+    })
+    .catch(() => {
+      // User is not authenticated, allow access to route
+      next({ name: "home" });
+    });
+};
+
+const requireNoAuth = (to, from, next) => {
+  Auth.currentAuthenticatedUser()
+    .then(() => {
+      // User is authenticated, redirect to analysis page
+      next({ name: "analyse" });
+    })
+    .catch(() => {
+      // User is not authenticated, allow access to route
+      next();
+    });
+};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,22 +54,39 @@ const router = createRouter({
       path: "/home",
       name: "home",
       component: Home,
+      beforeEnter: requireNoAuth,
     },
     {
-      path: "/analyse",
-      name: "analyse",
-      component: Analyse,
+      path: "/login",
+      name: "login",
+      component: Login,
+      beforeEnter: requireNoAuth,
     },
     {
-      path: "/eval",
-      name: "eval",
-      component: Eval,
-      props: true,
+      path: "/user",
+      name: "user",
+      component: User,
+      beforeEnter: requireAuth,
+      children: [
+        {
+          path: "analyse",
+          name: "analyse",
+          component: Analyse,
+          beforeEnter: requireAuth,
+        },
+        {
+          path: "eval",
+          name: "eval",
+          component: Eval,
+          props: true,
+          beforeEnter: requireAuth,
+        },
+      ],
     },
     // Fallback
     {
       path: "/:pathMatch(.*)*",
-      redirect: { name: "evaluation" },
+      redirect: { name: "home" },
     },
   ],
 });
---
+++ src/views/user/IndexUser.vue
@@ -0,0 +1,30 @@
+<script setup>
+import '@aws-amplify/ui-vue/styles.css';
+import { RouterView } from "vue-router";
+import MiniNavBar from '../../components/user/MiniNavBar.vue'
+import TopRight from '../../components/user/TopRight.vue'
+import { useAuthenticator } from '@aws-amplify/ui-vue';
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+
+import { Amplify } from 'aws-amplify';
+import awsconfig from '../../aws-exports';
+
+Amplify.configure(awsconfig);
+const auth = useAuthenticator();
+</script>
+
+<template>
+    <authenticator>
+        <nav>
+
+            <TopRight :auth="auth" />
+            <MiniNavBar :auth="auth" />
+        </nav>
+        <div class="container mx-auto">
+            <div class="flex flex-col flex-grow">
+                <router-view />
+            </div>
+        </div>
+    </authenticator>
+</template>
