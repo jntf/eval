9c3a3e45e497b032b1584faacafb95698ddff6ab
julien <107534950+jntf@users.noreply.github.com>
2023-05-25 09:35:11 +0200

    various

--- src/views/user/History.vue
+++ src/views/user/History.vue
@@ -138,7 +138,9 @@ export default {
             const day = String(date.getDate()).padStart(2, '0');
             const month = String(date.getMonth() + 1).padStart(2, '0');
             const year = date.getFullYear();
-            return `${day}/${month}/${year}`;
+            const hour = String(date.getHours()).padStart(2, '0');
+            const minute = String(date.getMinutes()).padStart(2, '0')
+            return `${hour}:${minute} - ${day}/${month}/${year}`;
         };
 
         const formatArray = (array) => {
@@ -220,7 +222,7 @@ export default {
             try {
                 const response = await Storage.list('');
                 const files = response.results;
-                const filesFiltered = files.filter((file) => file.key.includes('result'))
+                const filesFiltered = files.filter((file) => file.key.includes('result.xlsx'))
 
                 const filePromises = filesFiltered.map(async (file) => {
                     const datetimeParts = file.key.split('/')[0].split('-');
